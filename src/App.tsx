ef9a2d8f832481acf033718ef978406ade587873
Paulo Firmino <paulo.firmino@vr.com.br>
2025-04-14 02:44:52 -0300

    feat: add main layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import { useState } from "react";
+import { Footer } from "./components/Footer";
+import Header from "./components/Header/Header";
+import { ChatBubbleIcon } from "@radix-ui/react-icons";
+import { Chat } from "./components/Chat";
 function App() {
+  const [isChatOpen, setIsChatOpen] = useState(false);
+
+  const toggleChat = () => setIsChatOpen(!isChatOpen);
   return (
-    <div className="mt-7">
-      <h1 className="text-3xl font-bold underline">Hello vite</h1>
+    <div
+      data-testid="app-root"
+      className="flex flex-col justify-between h-screen w-screen"
+    >
+      <Header />
+      <main className="flex-grow" data-testid="main">
+        <div className="mx-auto p-4">
+          <h1 className="text-2xl font-bold mb-4">
+            Welcome to Fintalk Challenge
+          </h1>
+          <p>This is where your main content will go.</p>
+        </div>
+      </main>
+      <Footer />
+      <button
+        data-testid="chat-button"
+        onClick={toggleChat}
+        className="cursor-pointer fixed bottom-5 sm:bottom-25 right-6 bg-pink-500 hover:bg-pink-600 dark:bg-gray-700 dark:hover:bg-gray-600 text-white p-3 rounded-full shadow-lg transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-pink-500 dark:focus:ring-gray-400 focus:ring-opacity-50"
+        aria-label="Open chat"
+      >
+        <ChatBubbleIcon data-testid="chat-icon" className="h-6 w-6" />
+      </button>
+      {isChatOpen && <Chat onClose={() => setIsChatOpen(false)} />}
     </div>
   );
 }
